2253204487ceb63ff47f94a304adc4f84c0ddfc2
rm-21 <mittalrishabh181@gmail.com>
2022-04-21 11:28:45 +0530

    Added code to generate random number

--- src/part2.rs
+++ src/part2.rs
@@ -1,8 +1,22 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
-    //PART 1
     println!("Guess the Number!");
+
+    /*
+    - thread_rng()
+        --> gives the random number generator
+        --> Local to the current thread of execution
+        --> Tied to seed of the OS
+    - gen_range()
+        --> Defined by `Rng` trait
+        --> how to define a range --> start..end --> start included, end excluded
+        --> Or we could use start..=end --> 1..=100
+    */
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
+
     println!("Please provide a guess!");
 
     let mut guess = String::new();
@@ -12,6 +26,4 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed {}", guess);
-
-    //PART 2
 }
